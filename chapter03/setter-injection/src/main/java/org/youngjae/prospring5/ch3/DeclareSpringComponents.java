d2325976249dac3653f3e657ebea05194f24a2e2
hwangyoungjae <dudwo56@gmail.com>
2021-10-26 14:49:49 +0900

    DeclareSpringComponents

--- chapter03/setter-injection/src/main/java/org/youngjae/prospring5/ch3/DeclareSpringComponents.java
+++ chapter03/setter-injection/src/main/java/org/youngjae/prospring5/ch3/DeclareSpringComponents.java
@@ -1,6 +1,16 @@
 package org.youngjae.prospring5.ch3;
 
+import org.springframework.context.support.GenericXmlApplicationContext;
+import org.youngjae.prospring5.ch2.decoupled.MessageRenderer;
+
 public class DeclareSpringComponents {
     public static void main(String[] args) {
+        GenericXmlApplicationContext ctx = new GenericXmlApplicationContext();
+        ctx.load("classpath:spring/app-context-xml.xml");
+        ctx.refresh();
+        MessageRenderer messageRenderer = ctx.getBean("renderer",
+                MessageRenderer.class);
+        messageRenderer.render();
+        ctx.close();
     }
 }
